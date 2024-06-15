43b04e9a9a4e79321352c92df86896bf30dadf88
IG <11618271+stereoknife@users.noreply.github.com>
2024-06-15 22:46:37 +0200

    move main to src

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include <iostream>
-#include <glad/glad.h>
+#include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
