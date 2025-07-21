cf898c7c8ca6a88abbf4ad55f6f682b6c19c60fd
IG <11618271+stereoknife@users.noreply.github.com>
2025-07-21 11:58:18 +0200

    rename project

--- src/UI.cpp
+++ src/UI.cpp
@@ -8,6 +8,7 @@
 #include "Root.h"
 #include "imgui_impl_opengl3.h"
 
+//TODO: Add frame time overlay
 auto UI::draw() -> void {
     const ImGuiViewport* viewport = ImGui::GetMainViewport();
 
@@ -36,6 +37,9 @@ auto UI::draw() -> void {
     ImGui::SetNextWindowSizeConstraints(ImVec2{0, height}, ImVec2{width, height});
     ImGui::SetNextWindowSize(ImVec2(200, viewport->Size.y), ImGuiCond_FirstUseEver);
     ImGui::Begin("Properties", nullptr, ImGuiWindowFlags_NoMove);
+
+    // Transform // Disabled for now since meshes can't be moved yet
+    /*
     ImGui::SeparatorText("Position");
     static float x = 0.001f;
     static float y = 0.001f;
@@ -43,6 +47,12 @@ auto UI::draw() -> void {
     ImGui::InputFloat("X", &x, 0.01f, 1.0f, "%.3f");
     ImGui::InputFloat("Y", &y, 0.01f, 1.0f, "%.3f");
     ImGui::InputFloat("Z", &z, 0.01f, 1.0f, "%.3f");
+    */
+
+    // Shader properties
+    ImGui::SeparatorText("Material");
+    static s32 n = 0;
+    ImGui::Combo("Shader", &n, "Phong\0PBR\0IBL\0\0");
     ImGui::End();
 
     ImGui::ShowDemoWindow();
